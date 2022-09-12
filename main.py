adfd1053e0f099a03ee1b97c4dc51fc99137ab37
JiangY <sd843206695@sina.com>
2022-09-12 12:29:15 +0800

    优化gui启动逻辑

--- main.py
+++ main.py
@@ -1,22 +1,29 @@
 import ctypes
 import sys
-from PyQt5 import QtWidgets, QtCore
-from gui.connect.connect_gui import mainUI
+from PyQt5 import QtCore
+from PyQt5.QtWidgets import QApplication
+from deskPage.deskGui.connect.connect_gui import mainUI
 
 
 def is_admin():
+    # python 提权方法 https://stackoverflow.com/questions/130763/request-uac-elevation-from-within-a-python-script
     try:
-        return ctypes.windll.shell32.IsUserAnAdmin()
+        if ctypes.windll.shell32.IsUserAnAdmin() == 1:
+            return True
+        return False
     except Exception as e:
         return False
 
 
 if __name__ == '__main__':
+
     if is_admin():
+        ctypes.windll.shcore.SetProcessDpiAwareness(False)  # 需要在渲染GUI前执行，False表示不启用获取显示器真是分辨率。得到的结果是缩放分辨率(例如4K进行缩放到150%的分辨率变成了2K)
         QtCore.QCoreApplication.setAttribute(QtCore.Qt.AA_EnableHighDpiScaling)
-        app = QtWidgets.QApplication(sys.argv)
+        app = QApplication(sys.argv)
         main_gui = mainUI()
-        main_gui.show()
+        main_gui.q.show()
         sys.exit(app.exec_())
     else:
-        ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, __file__, None, 1)
+        ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, " ".join(sys.argv[1:]), None, 0)  # pyinstaller 打包模式
+        # ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, " ".join(sys.argv), None, 0)  # 调试模式
