92e568c9e8d333e952c3261be9f24aede0699bf9
JiangY <37106059+moonlessdark@users.noreply.github.com>
2024-01-20 13:48:41 +0800

    Update findButton.py

--- DeskPage/DeskDance/findPicBySmallToBigger/findButton.py
+++ DeskPage/DeskDance/findPicBySmallToBigger/findButton.py
@@ -57,39 +57,41 @@ class FindButton:
                         button_key_list.append(kk)
         return button_key_list
 
-    def find_pic_by_bigger(self, bigger_pic, pic_size=None, find_type="团练") -> list:
+    def find_pic_by_bigger(self, bigger_pic_cap: PicCapture, find_type="团练", threshold: float = 0.9) -> list:
         """
         从大图里面找小图，并进行从左到右排序
+        :param threshold:
         :param find_type: 查找方式，团练 或者 望辉洲
-        :param pic_size: 图片大小 [high, width]
-        :param bigger_pic: 需要找的大图，是已经读取的图片
+        :param bigger_pic_cap: 需要找的大图，是已经读取的图片
         :return: list[str]
         """
-        h = pic_size[0]
-        w = pic_size[1]
-        threshold: float = 0.9
-        if pic_size is not None:
+        bigger_pic = bigger_pic_cap.pic_content
+        width = bigger_pic_cap.pic_width
+        height = bigger_pic_cap.pic_height
+        button_dict = {}
+
+        if width > 0 and height > 0:
             """
             裁剪一下区域，大致把按钮出现的区域放进来
             """
             if find_type == "团练":
-                bigger_pic = bigger_pic[int(h * 0.64):int(h * 0.85), int(w * 0.35):int(w * 0.65)]
+                bigger_pic = bigger_pic[int(height * 0.64):int(height * 0.85), int(width * 0.35):int(width * 0.65)]
                 threshold = 0.9
             else:
-                bigger_pic = bigger_pic[int(h * 0.70):int(h * 0.74), int(w * 0.40):int(w * 0.60)]
+                bigger_pic = bigger_pic[int(height * 0.70):int(height * 0.78), int(width * 0.40):int(width * 0.60)]
                 threshold = 0.65
+
             # cv2.imshow("dd", bigger_pic)
             # cv2.waitKey()
 
-        button_list: list = self.get_dance_pic() if find_type == "团练" else self.get_whz_dance_pic()
-        button_dict = {}
-        for i in button_list:
-            button_num_list: list = find_pic(i[1], bigger_pic, threshold)  # 去界面找一下按钮的坐标
-            button_x = []
-            for bu in button_num_list:
-                button_x.append(bu[0])  # 把获取到的按钮 x(横坐标) 拿出来，待会要进行排序使用
-            if len(button_x) > 0:
-                button_dict[i[0]] = button_x  # {J:[123,456], K:[234,567]}
+            button_list: list = self.get_dance_pic() if find_type == "团练" else self.get_whz_dance_pic()
+            for i in button_list:
+                button_num_list: list = find_pic(i[1], bigger_pic, threshold)  # 去界面找一下按钮的坐标
+                button_x = []
+                for bu in button_num_list:
+                    button_x.append(bu[0])  # 把获取到的按钮 x(横坐标) 拿出来，待会要进行排序使用
+                if len(button_x) > 0:
+                    button_dict[i[0]] = button_x  # {J:[123,456], K:[234,567]}
         return self.sort_button(button_dict)
 
 
