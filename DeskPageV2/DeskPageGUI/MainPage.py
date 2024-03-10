261bbab422939bf05b88877ff0169c4fde6dfd96
JiangY <37106059+moonlessdark@users.noreply.github.com>
2024-03-10 13:15:17 +0800

    执行时新增进度条和计算已经检测了多少轮按钮的UI

--- DeskPageV2/DeskPageGUI/MainPage.py
+++ DeskPageV2/DeskPageGUI/MainPage.py
@@ -1,7 +1,11 @@
 import ctypes
+import os
 import sys
+import time
+import winreg
 
-from PySide6 import QtWidgets, QtCore
+from PySide6 import QtWidgets, QtCore, QtGui
+from PySide6.QtCore import Qt
 from PySide6.QtGui import QIcon
 from PySide6.QtWidgets import QApplication
 
@@ -11,29 +15,78 @@ class MainGui(QtWidgets.QMainWindow):
     def __init__(self):
         super().__init__()
 
-        self.setFixedSize(280, 400)
-        self.setWindowTitle("蜗牛跳舞小助手")
+        self.setFixedSize(280, 420)
+        # self.setWindowTitle("蜗牛跳舞小助手")
         # 加载任务栏和窗口左上角图标
         self.setWindowIcon(QIcon("./_internal/Resources/logo/logo.ico"))
         ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID("myappid")
+
+        """
+        顶部菜单栏
+        """
+        menu_bar = self.menuBar()
+
+        file_menu = QtWidgets.QMenu("&配置", self)
+        about_menu = QtWidgets.QMenu("&帮助", self)
+
+        menu_bar.addMenu(file_menu)
+        menu_bar.addMenu(about_menu)
+
+        action_open_config_file = QtGui.QAction("打开配置文件", self)
+        file_menu.addAction(action_open_config_file)
+        action_open_config_file.triggered.connect(self.open_config_file)
+
+        action_open_url = QtGui.QAction("访问项目github", self)
+        about_menu.addAction(action_open_url)
+        action_open_url.triggered.connect(self.open_url_project)
+
+        action_open_url_get_fore_ground_window_fail = QtGui.QAction("修复窗户激活失败", self)
+        about_menu.addAction(action_open_url_get_fore_ground_window_fail)
+        action_open_url_get_fore_ground_window_fail.triggered.connect(self.open_url_get_fore_ground_window_fail)
+
         """
         底部状态栏显示区域
         """
         self.status_bar_print = QtWidgets.QStatusBar(self)
         self.setStatusBar(self.status_bar_print)
-        self.status_bar_print.showMessage(" 等待执行")
+
+        # 左侧区域
+        # 给状态栏显示文字用的
+        self.status_bar_label_left = QtWidgets.QLabel()
+        # 状态栏本身显示的信息 第二个参数是信息停留的时间，单位是毫秒，默认是0（0表示在下一个操作来临前一直显示）
+        # 在状态栏左侧新增显示控件
+        self.status_bar_print.addWidget(self.status_bar_label_left, stretch=1)
+        # 加载一个进度条
+        self.progress_bar = QtWidgets.QProgressBar(self)
+        self.progress_bar.setInvertedAppearance(False)  # 进度条的走向
+        self.progress_bar.setOrientation(QtCore.Qt.Orientation.Horizontal)  # 进度条的方向
+        # 出现跑马灯的效果
+        self.progress_bar.setMinimum(0)
+        self.progress_bar.setMaximum(0)
+        self.progress_bar.setFixedHeight(15)
+        self.progress_bar.setAlignment(Qt.AlignmentFlag.AlignCenter)
+
+        self.status_bar_print.addWidget(self.progress_bar, stretch=3)
+
+        # 右侧区域
+        self.status_bar_label_right = QtWidgets.QLabel()
+        self.status_bar_label_right.setText("一共识别了 0 轮")
+        self.status_bar_label_right.setAlignment(Qt.AlignmentFlag.AlignRight)
+        self.status_bar_print.addWidget(self.status_bar_label_right, stretch=1)
+
+        self.status_bar_print.setContentsMargins(8, 0, 0, 0)
 
         """
         日志打印区      
         """
         self.text_browser_print_log = QtWidgets.QTextBrowser(self)
-        self.text_browser_print_log.setGeometry(QtCore.QRect(10, 190, 260, 188))
+        self.text_browser_print_log.setGeometry(QtCore.QRect(10, 210, 260, 188))
 
         """
         选择类型
         """
         self.group_box_functional_area = QtWidgets.QGroupBox(self)  # 功能区
-        self.group_box_functional_area.setGeometry(QtCore.QRect(10, 5, 260, 60))
+        self.group_box_functional_area.setGeometry(QtCore.QRect(10, 30, 260, 60))
         self.group_box_functional_area.setTitle("选择功能")
 
         self.radio_button_school_dance = QtWidgets.QRadioButton()
@@ -65,7 +118,7 @@ class MainGui(QtWidgets.QMainWindow):
         选择游戏窗口与执行
         """
         self.group_box_get_windows = QtWidgets.QGroupBox(self)
-        self.group_box_get_windows.setGeometry(QtCore.QRect(10, 70, 260, 70))
+        self.group_box_get_windows.setGeometry(QtCore.QRect(10, 90, 260, 70))
         self.group_box_get_windows.setTitle("选择游戏窗口")
 
         # 获取窗口按钮
@@ -90,7 +143,7 @@ class MainGui(QtWidgets.QMainWindow):
 
         # 获取到的游戏窗口要如何加载
         self.group_box_select_windows = QtWidgets.QGroupBox(self)
-        self.group_box_select_windows.setGeometry(QtCore.QRect(10, 140, 260, 40))
+        self.group_box_select_windows.setGeometry(QtCore.QRect(10, 160, 260, 40))
 
         # 窗口A/B/C
         self.check_box_windows_one = QtWidgets.QCheckBox()
@@ -112,6 +165,34 @@ class MainGui(QtWidgets.QMainWindow):
         self.layout_group_box_select_windows.addWidget(self.check_box_windows_three)
         self.layout_group_box_select_windows.addStretch(1)
         self.setLayout(self.layout_group_box_select_windows)
+
+    def open_config_file(self):
+        """
+        打开配置文件
+        :return:
+        """
+        config_file: str = '.\\_internal\\Resources\\'
+        if not os.path.exists(config_file):  # 如果主目录+小时+分钟这个文件路径不存在的话
+            config_file = ".\\DeskPageV2\\Resources\\"
+        QtWidgets.QFileDialog.getOpenFileName(self, "资源文件", config_file,
+                                              "Text Files (*.yaml;*.bat;*.png;*.ico;*.dll)")
+
+    @staticmethod
+    def open_url_get_fore_ground_window_fail(self):
+        """
+        激活窗口失败
+        :param self:
+        :return:
+        """
+        QtGui.QDesktopServices.openUrl(QtCore.QUrl("https://blog.csdn.net/qq_26013403/article/details/129122971"))
+
+    @staticmethod
+    def open_url_project():
+        """
+        打开网页
+        :return:
+        """
+        QtGui.QDesktopServices.openUrl(QtCore.QUrl("https://github.com/moonlessdark/JiuYinDance"))
 
     def set_ui_load_windows_check_box_init(self):
         """
@@ -148,7 +229,6 @@ class MainGui(QtWidgets.QMainWindow):
 
 
 if __name__ == '__main__':
-
     app = QApplication(sys.argv)
     main_gui = MainGui()
     main_gui.show()
