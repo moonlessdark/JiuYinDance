aefb3bafbd881928106b80fdebff249a1385fa8a
JiangY <37106059+moonlessdark@users.noreply.github.com>
2024-03-10 13:29:26 +0800

    Update dataClass.py

--- DeskPageV2/Utils/dataClass.py
+++ DeskPageV2/Utils/dataClass.py
@@ -3,6 +3,7 @@ import dataclasses
 
 @dataclasses.dataclass
 class DancePic:
+    dance_area: str = dataclasses.field(default_factory=str)
     dance_J: str = dataclasses.field(default_factory=str)
     dance_K: str = dataclasses.field(default_factory=str)
     dance_L: str = dataclasses.field(default_factory=str)
@@ -14,6 +15,7 @@ class DancePic:
 
 @dataclasses.dataclass
 class WhzDancePic:
+    dance_area: str = dataclasses.field(default_factory=str)
     dance_Down: str = dataclasses.field(default_factory=str)
     dance_Up: str = dataclasses.field(default_factory=str)
     dance_Left: str = dataclasses.field(default_factory=str)
@@ -29,3 +31,9 @@ class DmDll:
 @dataclasses.dataclass
 class GhostDll:
     dll_ghost: str = dataclasses.field(default_factory=str)
+
+
+@dataclasses.dataclass
+class Config:
+    dance_threshold: float = dataclasses.field(default_factory=float)
+    whz_dance_threshold: float = dataclasses.field(default_factory=float)
