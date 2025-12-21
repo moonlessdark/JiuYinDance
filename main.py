0d330993d3ac4eb223921cbb634c9afeb2a42d9e
JiangY <sd843206695@sina.com>
2025-12-21 21:58:30 +0800

    UP

--- main.py
+++ main.py
@@ -13,9 +13,8 @@ if __name__ == '__main__':
     app.setStyle("Fusion")
     w = TaskConnect()
 
-    w.log_print("\n\n更新日期: 2025-12-13\n"
+    w.log_print("\n\n更新日期: 2025-12-21\n"
                 "更新内容: \n"
-                "玄机报名时检测当前是否组队\n"
-                "每天首次押镖时自动接取每日任务\n")
+                "   新增农夫自动种植(测试版)\n")
     w.show()
     app.exec()
